9925c20f46f627d8d9055d4c961070df40bad354
Leonie <leonie.heron@ispm.unibe.ch>
2021-08-23 14:24:15 +0200

    update supplementary files

---
+++ SF_Q1_by_precision.R
@@ -0,0 +1,80 @@
+###############################################################
+# Supplementary material                        ##########
+# Q1 - studies ordered by precision                   ##########
+###############################################################
+
+
+#load the libraries
+library(meta)
+library(readxl)
+library(tidyverse)
+library(httr) # use to retrieve data from REDCap
+
+# Prepare the data
+# get the data directly from redcap:
+# report #155 is Q1:
+url <- "https://redcap.ispm.unibe.ch/api/"
+token <- "F2725F15FE84D2832E2793BB23B0A62B"
+formData <- list("token"=token,
+                 content='report',
+                 format='csv',
+                 report_id='155',
+                 csvDelimiter='',
+                 rawOrLabel='raw',
+                 rawOrLabelHeaders='raw',
+                 exportCheckboxLabel='false',
+                 returnFormat='csv'
+)
+response <- httr::POST(url, body = formData, encode = "form")
+asymptomaticQ1 <- httr::content(response)
+
+# minor cleaning
+asymptomaticQ1[asymptomaticQ1 == 9999] = NA
+
+asymptomaticQ1$setting = asymptomaticQ1$setting2
+
+
+data_long1 <- gather(asymptomaticQ1, cluster, total, c(q1_c1_total,q1_c2_total,q1_c3_total), factor_key=TRUE) %>% 
+  mutate(id=1:nrow(.)) %>%
+  select(record_id, author_1, total, id, cluster)
+data_long2 <-gather(asymptomaticQ1, cluster, events, c(q1_c1_event,q1_c2_event,q1_c3_event), factor_key=TRUE) %>% 
+  mutate(id=1:nrow(.)) %>%
+  select(events, id)
+
+data_Q1 = merge(data_long1, data_long2, by="id")
+data_Q1 = data_Q1[!is.na(data_Q1$total),]
+data_Q1$cluster = factor(data_Q1$cluster, labels = c("1","2","3"), levels = c("q1_c1_total", "q1_c2_total", "q1_c3_total"))
+
+data_Q1[data_Q1$record_id %in% asymptomaticQ1[is.na(asymptomaticQ1$q1_c2_event),]$record_id,]$cluster=NA
+
+asymptomaticQ1 <- data_Q1
+
+asymptomaticQ1$label=paste0(asymptomaticQ1$author_1, " (2020)",
+                            ifelse(!is.na(asymptomaticQ1$cluster),
+                                   paste0(" cluster:",asymptomaticQ1$cluster),
+                                   "")) # [FU: ",asymptomaticQ1$fup_median,"]")
+
+asymptomaticQ1$label=paste0(asymptomaticQ1$author_1,
+                            ifelse(!is.na(asymptomaticQ1$cluster),
+                                   paste0(" [cluster:",asymptomaticQ1$cluster,
+                                          "]"),"")) # [FU: ",asymptomaticQ1$fup_median,"]")
+
+#data <- asymptomaticQ1[order(asymptomaticQ1$setting,1/(1/asymptomaticQ1$events+1/(asymptomaticQ1$total-asymptomaticQ1$events))),]
+
+asym_plot_by_precision <- metaprop(events, total, data = asymptomaticQ1, sm = "PLOGIT", 
+                                   studlab = label)
+pdf(file = 'forest_meta_Q1_precision.pdf', width = 10, height = 27) 
+
+#forest(asym_plot_by_precision, sortvar = -seTE)
+forest(asym_plot_by_precision, sortvar = (1/(sqrt(seTE))))
+
+
+dev.off()
+
+png(file = 'forest_meta_Q1_precision.png', res = 300, height = 60, width = 20, units = "cm")
+
+forest(asym_plot_by_precision, sortvar = (1/(sqrt(seTE))))
+
+dev.off()
+
+
---
+++ SF_Q2.1_by_precision.R
@@ -0,0 +1,111 @@
+###############################################################
+# Question 2.1: results output ################################
+# Secondary attack rate from asymp or pre-symp cases ##########
+###############################################################
+
+
+#load libraries
+library(RCurl)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(meta)
+library(metafor)
+library(flextable)
+
+# get the data directly from redcap: 
+# report #172 is Q2.1 SAR:
+url <- "https://redcap.ispm.unibe.ch/api/"
+token <- "F2725F15FE84D2832E2793BB23B0A62B"
+formData2_1 <- list("token"=token,
+                    content='report',
+                    format='csv',
+                    report_id='172',
+                    csvDelimiter='',
+                    rawOrLabel='raw',
+                    rawOrLabelHeaders='raw',
+                    exportCheckboxLabel='false',
+                    returnFormat='json'
+)
+response2_1 <- httr::POST(url, body = formData2_1, encode = "form")
+asymptomaticQ2_1 <- httr::content(response2_1)
+
+#clean data
+asymptomaticQ2_1[asymptomaticQ2_1=="9999;9999"]=NA #indicate as missing
+asymptomaticQ2_1[asymptomaticQ2_1=="9999"]=NA #indicate as missing
+asymptomaticQ2_1=asymptomaticQ2_1%>% #separate symp SAR into 2 variables
+  separate(q3_sar_s, c("Ec","Nc"), ";", remove=FALSE) 
+asymptomaticQ2_1=asymptomaticQ2_1%>% #separate asymp SAR into 2 variables
+  separate(q3_sar_a, c("Ee_a","Ne_a"), ";", remove=FALSE) 
+asymptomaticQ2_1=asymptomaticQ2_1%>% #separate presymp SAR into 2 variables
+  separate(q3_sar_p, c("Ee_p","Ne_p"), ";", remove=FALSE) 
+
+asymptomaticQ2_1=asymptomaticQ2_1 %>% #change values to numeric
+  mutate(Ec=as.numeric(Ec),
+         Nc=as.numeric(Nc),
+         Ee_a=as.numeric(Ee_a),
+         Ne_a=as.numeric(Ne_a),
+         Ee_p=as.numeric(Ee_p),
+         Ne_p=as.numeric(Ne_p))
+
+#create new df with asymp/presymp as subgroups
+q2_1_df_a <- asymptomaticQ2_1 %>%
+  select("record_id", "author_1", "Ee_a", "Ne_a", "Ec", "Nc", "region", "year") %>%
+  rename(Ee = Ee_a,
+         Ne = Ne_a) %>%
+  filter(!is.na(Ee)) %>%
+  mutate(group = "Asymptomatic")
+q2_1_df_p <- asymptomaticQ2_1 %>%
+  select("record_id", "author_1", "Ee_p", "Ne_p", "Ec", "Nc", "region", "year") %>%
+  rename(Ee = Ee_p,
+         Ne = Ne_p) %>%
+  filter(!is.na(Ee)) %>%
+  mutate(group = "Presymptomatic")
+#bind rows
+q2_1_df <- bind_rows(q2_1_df_a, q2_1_df_p)
+
+
+#metaanalysis for asymptomatic transmission
+
+q2_1_asymp <- metabin(event.e = Ee,
+                      n.e = Ne,
+                      event.c = Ec,
+                      n.c = Nc, 
+                      data = q2_1_df,
+                      studlab = author_1,
+                      prediction = TRUE,
+                      sm = "RR",
+                      method = "MH",
+                      MH.exact = TRUE, 
+                      byvar = group)
+
+##########################################
+# supplementary information - by precision
+##########################################
+
+#metaanalysis for asymptomatic transmission
+
+q2_1_asymp_precision <- metabin(event.e = Ee,
+                                n.e = Ne,
+                                event.c = Ec,
+                                n.c = Nc, 
+                                data = q2_1_df,
+                                studlab = author_1,
+                                sm = "RR",
+                                byvar = group)
+
+pdf(file = 'forest_meta_Q2_1_precision.pdf', width = 10, height = 27) 
+
+forest(q2_1_asymp_precision, sortvar = (1/(sqrt(seTE))))
+
+
+dev.off()
+
+png(file = 'forest_meta_Q2_1_precision.png', res = 300, height = 14, width = 40, units = "cm")
+
+forest(q2_1_asymp_precision, sortvar = (1/(sqrt(seTE))))
+
+dev.off()
+
+
+
