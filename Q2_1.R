91f56e3a28352d9fbb749dc7e975c20dedf72c54
leonieheron <60140916+leonieheron@users.noreply.github.com>
2021-12-23 13:56:18 +0100

    Add files via upload

--- Q2_1.R
+++ Q2_1.R
@@ -19,8 +19,7 @@ library(metafor)
 # get the data directly from redcap: 
 # report #172 is Q2.1 SAR:
 url <- "https://redcap.ispm.unibe.ch/api/"
-#APIs token is not sharable
-token <- "##################################"
+token <- "F2725F15FE84D2832E2793BB23B0A62B"
 formData2_1 <- list("token"=token,
                     content='report',
                     format='csv',
@@ -48,6 +47,8 @@ asymptomaticQ2_1=asymptomaticQ2_1%>% #separate asymp SAR into 2 variables
 asymptomaticQ2_1=asymptomaticQ2_1%>% #separate presymp SAR into 2 variables
   separate(q3_sar_p, c("Ee_p","Ne_p"), ";", remove=FALSE) 
 
+
+
 asymptomaticQ2_1=asymptomaticQ2_1 %>% #change values to numeric
   mutate(Ec=as.numeric(Ec),
          Nc=as.numeric(Nc),
@@ -72,15 +73,57 @@ q2_1_df_p <- asymptomaticQ2_1 %>%
 #bind rows
 q2_1_df <- bind_rows(q2_1_df_a, q2_1_df_p)
 
-#cut data at end of jan 31
-published_preprints <-c(5565,6219, 6685, 7030, 7465, 8249, 9442, 9484)
-q2_1_df <- q2_1_df %>%
-  filter(record_id <= 5296 | record_id %in% published_preprints)
+#rename groups
+q2_1_df$group[q2_1_df$group == "Presymptomatic"] <- "Presymptomatic vs. Symptomatic"
+q2_1_df$group[q2_1_df$group == "Asymptomatic"] <- "Asymptomatic vs. Symptomatic"
+q2_1_df$group[q2_1_df$record_id == 821] <- "Asymptomatic vs. Presymptomatic"
+
+
 
 #add columns for forest plot
 q2_1_df$col_1 <- paste0(q2_1_df$Ee, "/", q2_1_df$Ne, sep = "")
 
 q2_1_df$col_2 <- paste0(q2_1_df$Ec, "/", q2_1_df$Nc, sep = "")
+
+
+#run meta analysis for all available studies
+#metaanalysis for asymptomatic transmission
+
+q2_1_asymp <- metabin(event.e = Ee,
+                      n.e = Ne,
+                      event.c = Ec,
+                      n.c = Nc, 
+                      data = q2_1_df,
+                      studlab = author_1,
+                      prediction = TRUE,
+                      sm = "RR",
+                      method = "MH",
+                      hakn = TRUE,
+                      MH.exact = TRUE, 
+                      byvar = group,
+                      comb.random = TRUE,
+                      comb.fixed = FALSE,
+                      print.byvar = FALSE,
+                      label.e = "",
+                      label.c = "Symp.  ")
+q2_1_asymp
+
+
+tiff(filename = "Q2_1_update5.tiff",
+     width = 2450, height = 1400,
+     res = 300)
+forest(q2_1_asymp, sortvar = (1/(sqrt(seTE))), overall = FALSE,
+       overall.hetstat = FALSE,
+       print.byvar = FALSE,
+       leftcols = c("studlab", "col_1", "col_2"),
+       leftlabs = c("Author", "E/N", "E/N (REF)"))
+dev.off()
+
+
+#cut data at end of jan 31
+published_preprints <-c(5565,6219, 6685, 7030, 7465, 8249, 9442, 9484)
+q2_1_df <- q2_1_df %>%
+  filter(record_id <= 5296 | record_id %in% published_preprints)
 
 #metaanalysis for asymptomatic transmission
 
@@ -97,6 +140,7 @@ q2_1_asymp <- metabin(event.e = Ee,
                       MH.exact = TRUE, 
                       byvar = group,
                       comb.random = TRUE,
+                      print.byvar = FALSE,
                       comb.fixed = FALSE, 
                       label.e = "",
                       label.c = "Symp.  ")
@@ -104,11 +148,24 @@ q2_1_asymp
 
 
 tiff(filename = "Q2_1.tiff",
-     width = 2450, height = 1400,
+     width = 2450, height = 1600,
      res = 300)
 forest(q2_1_asymp, sortvar = (1/(sqrt(seTE))), overall = FALSE,
        overall.hetstat = FALSE,
        print.byvar = FALSE,
+       addrows.below.overall = 2,
        leftcols = c("studlab", "col_1", "col_2"),
        leftlabs = c("Author", "E/N", "E/N (Symp.)"))
 dev.off()
+
+
+pdf("Q2_1.pdf",
+     width = 8, height = 6)
+forest(q2_1_asymp, sortvar = (1/(sqrt(seTE))), overall = FALSE,
+       overall.hetstat = FALSE,
+       print.byvar = FALSE,
+       addrows.below.overall = 2,
+       leftcols = c("studlab", "col_1", "col_2"),
+       leftlabs = c("Author", "E/N", "E/N (Symp.)"))
+dev.off()
+
