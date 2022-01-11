02c466c74d12af283cf0758e13d276a81c30e709
HiraImeri <49593665+HiraImeri@users.noreply.github.com>
2022-01-11 17:04:57 +0100

    Update getDataREDCap.R

--- getDataREDCap.R
+++ getDataREDCap.R
@@ -17,3 +17,19 @@ formData <- list("token"=token,
                  exportCheckboxLabel='false',
                  returnFormat='csv'
 )
+
+
+# get the ROB data directly from redcap:
+urlrob <- "https://redcap.ispm.unibe.ch/api/"
+#APIs token is not sharable
+token <- "##################################"
+formDatarob <- list("token"=tokenrob,
+                    content='report',
+                    format='csv',
+                    report_id='283',
+                    csvDelimiter='',
+                    rawOrLabel='raw',
+                    rawOrLabelHeaders='raw',
+                    exportCheckboxLabel='false',
+                    returnFormat='csv'
+)
